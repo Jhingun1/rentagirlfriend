f9c754c4bc50afd01bb711e0d3b5dc728b330648
Chingun Erdene Ochir <i.quietus@gmail.com>
2024-07-09 00:03:19 +1000

    quick fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { Landing as Landing } from "../components/Landing";
+import { Landing } from '@/components/Landing';
 
 export default function Home() {
   return (
-    <Landing>
-    </Landing>
+    <Landing />
   );
 }
